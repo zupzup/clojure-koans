8d5fc2290552baa4f09faa8efb186b02717f4867
Mario Zupan <mzupanmz@gmail.com>
2015-04-04 03:51:00 +0200

    atoms

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -3,21 +3,26 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
-  (loop [n   n
-         acc true]
+  (loop [n n acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (if (= 1 (count coll))
+    coll
+    (conj (recursive-reverse (drop-last coll)) (last coll))
+    ))
 
 (defn factorial [n]
-  __)
+  (loop [n n acc 1]
+    (if (= n 1)
+      acc
+      (recur (dec n) (* acc n)))))
 
 (meditations
   "Recursion ends with a base case"
